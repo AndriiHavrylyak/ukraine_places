506e809bfd5ec5456baeddf4bd24ac2baa456668
AndriiHavrylyak <andreigavrylyak2@gmail.com>
2023-05-20 08:33:04 +0300

    add api for places

--- lib/core/config/fb_dynamic_links_params.dart
+++
@@ -1,7 +0,0 @@
-class FirebaseDynamicLinksParams {
-  static const link = 'https://riding-rainbow-dev.web.app/';
-  static const uriPrefix = 'https://ridingtherainbow.page.link';
-  static const packageName = 'com.riding_rainbow';
-  static const bundleId = 'com.ridingrainbow';
-
-}
